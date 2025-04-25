d47aa5e5419425796b15a35fc62a9f978739ee9d
Zachery <zachery.meck@gmail.com>
2025-04-25 16:10:30 +0200

    Created the basic Console menu and the function for subtraction and addition.

--- Program.cs
+++ Program.cs
@@ -10,6 +10,34 @@ namespace console_basic_calculator
     {
         static void Main(string[] args)
         {
+            // Input
+            Console.WriteLine("Enter the first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            // Console Menu
+            Console.WriteLine("Select an option:");
+            Console.WriteLine("\n1.Addition");
+            Console.WriteLine("\n2.Subtraction");
+            Console.WriteLine("\n3.Multiplication");
+            Console.WriteLine("\n4.Division");
+            Console.WriteLine("\n5.Exit");
+            Console.Write("Your option: ");
+
+            // Formula
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Console.WriteLine($"Your result is: {num1} + {num2} = " + (num1 + num2));
+                    break;
+                case "2":
+                    Console.WriteLine($"Your result is: {num1} - {num2} = " + (num1 - num2));
+                    break;
+            }
+            // Print out result
+
         }
     }
 }
